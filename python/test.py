2737a8a6e26ff35bd35feb743dfb238b7e2c237c
Januar Boediman <bygrace@Jannis-MacBook-Pro.local>
2020-06-23 20:19:51 +1000

    update content

--- python/test.py
+++ python/test.py
@@ -1,5 +1,10 @@
 msg = "Hello World"
-msg.capitalize
 print(msg)
-
-print("only this one")
+print('what is your name?') # ask for the name
+myname = input()
+print ('it is good to meet you, ' + myname)
+print ('the length of your name is: ')
+print (len(myname))
+print ('what is your age?') # ask for the age
+myage = input()
+print ('you will be ' + str(int(myage)+1) + ' in a year.')
